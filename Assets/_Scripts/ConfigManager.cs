ffebcbb027aeacf719afdf6dd12ad3b24c182d37
ThaiTuan4526 <ttuan4526.forwork@gmail.com>
2024-06-08 19:07:08 +0700

    update

--- Assets/_Scripts/ConfigManager.cs
+++ Assets/_Scripts/ConfigManager.cs
@@ -11,5 +11,21 @@ public class ConfigManager : MonoBehaviour
 	private void Awake()
 	{
 		Instance = this;
+		Object.DontDestroyOnLoad(base.gameObject);
+		path = Path.Combine(Application.dataPath, "Config.ini");
+		try
+		{
+			string contents = File.ReadAllText(path);
+			File.WriteAllText(path, contents);
+		}
+			catch
+			{
+				path = Path.Combine(Application.persistentDataPath, "AppConfig.ini");
+			}
+	}
+	
+	private void Start()
+	{
+		
 	}
 }
